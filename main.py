8490012dc3725c90f9c71f931bd1b0e492984d01
kevingo <kevingo75@gmail.com>
2019-04-23 23:41:31 +0800

    Add main

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
 from gensim import models
+from collections import defaultdict
 
+documents = [
+    "Hello this is first doc",
+    "oops this is second doc"
+]
+
+# remove common words and tokenize
+stoplist = set('for a of the and to in'.split())
+texts = [
+    [word for word in document.lower().split() if word not in stoplist]
+    for document in documents
+]
+
+# remove words that appear only once
+frequency = defaultdict(int)
+for text in texts:
+    for token in text:
+        frequency[token] += 1
+
+texts = [
+    [token for token in text if frequency[token] > 1]
+    for text in texts
+]
+
+print(texts)
